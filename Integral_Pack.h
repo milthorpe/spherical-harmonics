96c6669ab9d3c787f8c91a96575ce360a7b691b0
josh <josh.milthorpe@gmail.com>
2014-09-15 16:45:22 -0400

    copyright headers for ANU code

--- Integral_Pack.h
+++ Integral_Pack.h
@@ -1,3 +1,13 @@
+/*  
+ * This file is part of ANUChem.
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ * (C) Copyright Australian National University 2012-2013.
+ */
 #ifndef __INTEGRAL_PACK_H_
 #define __INTEGRAL_PACK_H_
 
